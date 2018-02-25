b83205977985717f1acafb391291f78ec7695774
camerash <esmond9981127@gmail.com>
2018-02-25 22:06:55 +0800

    Completed search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,41 +1,10 @@
 import _ from 'lodash';
+import PropTypes from 'prop-types'
 import React, { Component } from 'react';
 import { Search } from 'semantic-ui-react'
+import source from '../assets/SearchData.json';
 
-const source = [
-  {
-    "title": "Bashirian, Sipes and Roob",
-    "description": "Diverse fault-tolerant ability",
-    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/cherif_b/128.jpg",
-    "price": "$99.09"
-  },
-  {
-    "title": "Eichmann LLC",
-    "description": "Grass-roots grid-enabled secured line",
-    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/skkirilov/128.jpg",
-    "price": "$90.45"
-  },
-  {
-    "title": "Ebert, Turner and D'Amore",
-    "description": "Progressive bandwidth-monitored open architecture",
-    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/michaelkoper/128.jpg",
-    "price": "$9.70"
-  },
-  {
-    "title": "Prosacco, White and Williamson",
-    "description": "Face to face value-added benchmark",
-    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/louis_currie/128.jpg",
-    "price": "$6.20"
-  },
-  {
-    "title": "Bernier Inc",
-    "description": "Customer-focused incremental utilisation",
-    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/samgrover/128.jpg",
-    "price": "$81.01"
-  }
-]
-
-class SearchBar extends Component {
+class SearchBarDefault extends Component {
   componentWillMount() {
     this.resetComponent()
   }
@@ -53,11 +22,18 @@ class SearchBar extends Component {
       if (this.state.value.length < 1) return this.resetComponent()
 
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
-      const isMatch = result => re.test(result.title)
+      const isMatch = result => re.test(result.name)
+
+      const filteredResults = _.reduce(source, (memo, data, name) => {
+        const results = _.filter(data.results, isMatch)
+        if (results.length) memo[name] = { name, results } // eslint-disable-line no-param-reassign
+
+        return memo
+      }, {})
 
       this.setState({
         isLoading: false,
-        results: _.filter(source, isMatch),
+        results: filteredResults,
       })
     }, 500)
   }
@@ -66,7 +42,8 @@ class SearchBar extends Component {
     const { isLoading, value, results } = this.state
 
     return (
-      <Search size='big' aligned='right'
+      <Search aligned='right'
+        category
         loading={isLoading}
         onResultSelect={this.handleResultSelect}
         onSearchChange={this.handleSearchChange}
@@ -77,5 +54,25 @@ class SearchBar extends Component {
     )
   }
 }
+
+const categoryRenderer = ({ name }) => <div className="name">Floor {name}</div>
+
+categoryRenderer.propTypes = {
+  name: PropTypes.string,
+}
+
+const resultRenderer = ({ name, lift }) => <div><div className="price">{ (lift != null && lift != "") ? "Lift " + lift : "" }</div><div className="title">{ name.match(/^\d/) ? "Room" + name : name }</div></div>
+
+resultRenderer.propTypes = {
+  name: PropTypes.string,
+  lift: PropTypes.string,
+}
+
+const SearchBar = () => (
+  <SearchBarDefault
+    categoryRenderer={categoryRenderer}
+    resultRenderer={resultRenderer}
+  />
+)
 
 export default SearchBar;
